ed7bae13765241d36a1b5af86855ea88cb0407b7
mialarisa9 <mia_larisa@apps.ipb.ac.id>
2019-05-12 20:15:45 +0700

    FE

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -1,7 +1,71 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Event</title>
 
-@section('content')
+      <!-- Google Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+       <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+
+     <!-- Custom styles for this template -->
+      <link href="{{ url('css/creative.min.css') }}" rel="stylesheet">
+      <link href="{{ url('css/modern-business.css') }}" rel="stylesheet">
+      <link href="css/scrolling-nav.css" rel="stylesheet">
+      <link rel="stylesheet" href="css/style.css">
+
+     <!-- Plugin CSS -->
+      <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+      
+</head>
+
+<body id="page-top">
+
+    <!-- Nav -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+        <div class="container">
+             <a class="navbar-brand js-scroll-trigger" href="{{ url('/') }}"><img class="img-top" src="/image/logo/you.png" width="100px"></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+            <div class="collapse navbar-collapse" id="navbarResponsive">   
+                <ul class="navbar-nav ml-auto  ml-auto my-2 my-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="{{ url('/') }}">Home </a>
+                    </li>
+                    
+            @guest                  
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Log in  </a>
+                    </li>
+            @if (Route::has('register'))
+                 
+            @endif
+            @else
+                </ul>
+            </div>                    
+                        <a id="navbarDropdown"  href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}<br> <span class="caret"></span>
+                        </a>
+                            <br>
+
+                        <a class="btn btn-primary" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                        </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+                    </form>
+            @endguest
+        </div>
+    </nav> 
 <div class="table-responsive">
+<br>
               <table id="mytable" class="table table-bordred table-striped">
                    
                    <thead>
@@ -55,7 +119,6 @@
                 {{-- // end loop --}}
             </tbody>
         </form>
-
+        
         
 </table>
-@endsection
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register!</title>
+    <title>Profile</title>
 
       <!-- Google Fonts -->
       <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
